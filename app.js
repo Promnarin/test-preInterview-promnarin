b2960d903ad3bb42a1004ce1a60473708efe08e1
Promnarin <103997839+Promnarin@users.noreply.github.com>
2022-04-19 17:02:59 +0700

    promnarin-test

---
+++ app.js
@@ -0,0 +1,11 @@
+var a=1200 , b=95 , c=110
+var d=a+b+c
+
+//เปอร์เซนที่ขายได้ของa
+var acar = (a/d)*100
+var bcar = (b/d)*100
+var ccar = (c/d)*100
+
+document.getElementById("result").innerHTML=acar
+document.getElementById("result1").innerHTML=bcar
+document.getElementById("result2").innerHTML=ccar
